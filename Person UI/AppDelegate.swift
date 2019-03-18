113ae1d07857243a6ed33ba265a409b02bc38fe2
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2019-03-18 16:49:58 -0500

    First commit

---
+++ Person UI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Person UI
+//
+//  Created by Brendan Krekeler on 3/18/19.
+//  Copyright © 2019 Brendan Krekeler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Person UI/PersonUIViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PersonUIViewController.swift
+//  Person UI
+//
+//  Created by Brendan Krekeler on 3/18/19.
+//  Copyright © 2019 Brendan Krekeler. All rights reserved.
+//
+
+import Cocoa
+
+class PersonUIViewController: NSViewController {
+
+    @IBOutlet weak var ageSliderCell: NSSliderCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        
+    }
+    
+    #warning("figure out how to change the text view background color based on accent color of the MAC")
+    
+    #warning("understand how to do tool tips")
+    
+}
